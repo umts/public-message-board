c6023cad57f680920575f6d5b630e08c3f7e20c3
benmelz <ben@melz.me>
2023-09-01 10:59:21 -0400

    add documentation

--- src/hooks/useDynamicHeight.js
+++ src/hooks/useDynamicHeight.js
@@ -1,7 +1,14 @@
 import {useEffect} from 'react';
 
 /**
- * TODO: Document.
+ * Communicates the height of the application to a parent window when embedded in an iframe, allowing for dynamic sizing
+ * of the iframe in the parent document.
+ *
+ * Through the ResizeObserver API, we watch for changes in the body's height, and forward them up to the parent document
+ * using the postMessage API, which allows for cross-origin communication through an iframe.
+ *
+ * It only sends the height data up, the parent document itself must then consume the data and size the iframe
+ * accordingly (see README for sample implementation).
  *
  * @return {undefined}
  */

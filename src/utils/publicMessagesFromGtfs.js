9fd4c97ac7a3806111f7b63fc7c095333f2b5093
benmelz <ben@melz.me>
2025-04-26 10:35:08 -0400

    implement sorting very messily

--- src/utils/publicMessagesFromGtfs.js
+++ src/utils/publicMessagesFromGtfs.js
@@ -1,10 +1,8 @@
 export default function publicMessagesFromGtfs (routesGtfs, alertsGtfs, routeFilter) {
   if (routesGtfs === null || alertsGtfs === null) return null
   if (routesGtfs === undefined || alertsGtfs === undefined) return undefined
-
   const routesGtfsMap = {}
   routesGtfs.forEach((routeGtfs) => { routesGtfsMap[routeGtfs.routeId] = routeGtfs })
-
   return alertsGtfs.filter((alertGtfs) => {
     if (routeFilter === null) return true
     if (alertGtfs.alert.informedEntity.length === 0) return true
@@ -12,18 +10,36 @@ export default function publicMessagesFromGtfs (routesGtfs, alertsGtfs, routeFil
       routeFilter.includes(routesGtfsMap[routeId].routeShortName)
     )
   }).sort((alertGtfs1, alertGtfs2) => {
-    return 0
+    const routeSortOrders1 = [...new Set(alertGtfs1.alert.informedEntity.map((entity) => (
+      entity.routeId
+    )))].map((routeId) => {
+      return routesGtfsMap[routeId]
+    }).map((route) => (route.routeSortOrder) ? parseInt(route.routeSortOrder) : Infinity)
+    const routeSortOrders2 = [...new Set(alertGtfs2.alert.informedEntity.map((entity) => (
+      entity.routeId
+    )))].map((routeId) => {
+      return routesGtfsMap[routeId]
+    }).map((route) => (route.routeSortOrder) ? parseInt(route.routeSortOrder) : Infinity)
+    const sortOrder1 = (routeSortOrders1.length > 0) ? Math.min(...routeSortOrders1) : Infinity
+    const sortOrder2 = (routeSortOrders2.length > 0) ? Math.min(...routeSortOrders2) : Infinity
+    return sortOrder1 - sortOrder2
   }).map((alertGtfs) => ({
     id: alertGtfs.id,
     header: alertGtfs.alert.headerText?.translation[0]?.text,
     description: alertGtfs.alert.descriptionText.translation[0].text,
-    routes: [...new Set(alertGtfs.alert.informedEntity.map((entity) => entity.routeId))].filter((routeId) => {
-      return routeFilter === null || routeFilter.includes(routesGtfsMap[routeId].routeShortName)
-    }).map((routeId) => ({
-      id: routeId,
-      abbreviation: routesGtfsMap[routeId].routeShortName,
-      color: routesGtfsMap[routeId].routeColor,
-      textColor: routesGtfsMap[routeId].textColor,
+    routes: [...new Set(alertGtfs.alert.informedEntity.map((entity) => entity.routeId))].map((routeId) => {
+      return routesGtfsMap[routeId]
+    }).filter((route) => {
+      return routeFilter === null || routeFilter.includes(route.routeShortName)
+    }).sort((route1, route2) => {
+      const sortOrder1 = (route1.routeSortOrder) ? parseInt(route1.routeSortOrder) : Infinity
+      const sortOrder2 = (route2.routeSortOrder) ? parseInt(route2.routeSortOrder) : Infinity
+      return sortOrder1 - sortOrder2
+    }).map((route) => ({
+      id: route.routeId,
+      abbreviation: route.routeShortName,
+      color: route.routeColor,
+      textColor: route.textColor,
     }))
   }))
 }

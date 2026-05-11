eeaa64eeb7fe9c7eda955c5a79892d2a69526f1c
benmelz <ben@melz.me>
2026-05-11 11:29:17 -0400

    add prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,34 @@
-import js from '@eslint/js'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
-import globals from 'globals'
+import js from "@eslint/js";
+import { defineConfig, globalIgnores } from "eslint/config";
+import prettier from "eslint-config-prettier";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import globals from "globals";
 
 export default defineConfig([
   globalIgnores(['dist']),
-  {languageOptions: { globals: globals.browser }},
-  js.configs.recommended,
-  reactHooks.configs.flat.recommended,
-  reactRefresh.configs.recommended,
+  {
+    files: ["**/*.{js,jsx}"],
+    extends: [
+      js.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.recommended,
+      prettier,
+    ],
+    settings: { react: { version: "detect" } },
+  },
+  {
+    files: ["src/**/*.{js,jsx}"],
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
+  {
+    files: ["src/**/*.jsx"],
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+      },
+    }
+  }
 ])

7d5794df0ffafed4e34f3ac060f2e1a7e723eea6
benmelz <ben@melz.me>
2026-05-11 11:15:27 -0400

    drop neostandard

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,13 @@
-import neostandard from 'neostandard'
+import js from '@eslint/js'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import { defineConfig, globalIgnores } from 'eslint/config'
+import globals from 'globals'
 
-export default [
-  { ignores: ['dist/*'] },
-  ...neostandard({ env: ['browser'] })
-]
+export default defineConfig([
+  globalIgnores(['dist']),
+  {languageOptions: { globals: globals.browser }},
+  js.configs.recommended,
+  reactHooks.configs.flat.recommended,
+  reactRefresh.configs.recommended,
+])
